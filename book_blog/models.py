230e6020dfc7bcc0d163c0ac14a444794a50beca
nathanieladiah <nathanieladiah@gmail.com>
2021-11-28 21:32:39 -0400

    Create database and save posts
    Created the fields in the database to store posts along with importing
    the django prebuilt User class. Automatically populate the index page
    with posts from the database.

--- book_blog/models.py
+++ book_blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Each blog post must have a title, a subtitle, author, post date, and the
+# post content itself
+
+class Post(models.Model):
+	title = models.CharField(max_length=120)
+	subtitle = models.CharField(max_length=200)
+	body = models.TextField()
+	date = models.DateField(default=date.today)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.title
--- book_blog/views.py
+++ book_blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
+from .models import Post
 
 # Create your views here.
 def index(request):
-	return render(request, "book_blog/index.html")
+	posts = Post.objects.order_by('-date').all()
+
+	return render(request, "book_blog/index.html", {
+		'posts': posts
+	})
