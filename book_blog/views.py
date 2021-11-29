6e334a8d06a56ff6db910e48d6bad898f596e122
nathanieladiah <nathanieladiah@gmail.com>
2021-11-28 22:33:04 -0400

    Add pagination to the index page

--- book_blog/views.py
+++ book_blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.contrib.auth.models import User
 
 from .models import Post
@@ -6,9 +7,13 @@ from .models import Post
 # Create your views here.
 def index(request):
 	posts = Post.objects.order_by('-date').all()
+	paginator = Paginator(posts, 5)
+
+	page_number = request.GET.get('page')
+	page_obj = paginator.get_page(page_number)
 
 	return render(request, "book_blog/index.html", {
-		'posts': posts
+		'page_obj': page_obj
 	})
 
 def post(request, post_id):
