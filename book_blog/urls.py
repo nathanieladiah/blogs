86d032ac615726ce12f23eac4a4e71752f88cdc9
nathanieladiah <nathanieladiah@gmail.com>
2021-12-04 18:16:57 -0400

    Create the new post template and views
    The new post page is a form that saves a title, subtitle and body
    formatted in markdown.
    
    Submitting the form saves the post as an entry in the posts table in the
    database.

--- book_blog/urls.py
+++ book_blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
 	path("post/<int:post_id>", views.post, name="post"),
 	path("about", views.about, name="about"),
 	path("contact", views.contact, name="contact"),
-	path("random", views.random, name="random")
+	path("random", views.random, name="random"),
+	path("new", views.new_post, name="new_post"),
 ]
--- book_blog/views.py
+++ book_blog/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.core.paginator import Paginator
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import user_passes_test
 
 from .models import Post
 
@@ -37,3 +38,17 @@ def random(request):
 	random_post = Post.objects.all()[randint(0, count-1)]
 
 	return HttpResponseRedirect(reverse("post", args=(random_post.id,)))
+
+@user_passes_test(lambda u: u.is_superuser)
+def new_post(request):
+	if request.method == 'POST':
+		title = request.POST['title']
+		subtitle = request.POST['subtitle']
+		body = request.POST['body']
+		author = request.user
+
+		post = Post(title=title, subtitle=subtitle, body=body, author=author)
+		post.save()
+		return HttpResponseRedirect(reverse('index'))
+
+	return render(request, "book_blog/new_post.html")
