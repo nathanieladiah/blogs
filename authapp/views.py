bc03a2cdb564b81a2086969403e47281b272c8d4
nathanieladiah <nathanieladiah@gmail.com>
2021-12-04 19:15:52 -0400

    Add login feature

--- authapp/views.py
+++ authapp/views.py
@@ -33,6 +33,26 @@ def register(request):
 	else:
 		return render(request, "authapp/register.html")
 
+def login_view(request):
+	if request.method == 'POST':
+
+		# Attempt to sign user in
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+
+		# check if authentication is successful
+		if user is not None:
+			login(request, user)
+			return HttpResponseRedirect(reverse('index'))
+		else:
+			return render(request, 'authapp/login.html', {
+				"message": "Invalid username and/or password."
+			})
+	
+	else:
+		return render(request, "authapp/login.html")
+
 def logout_view(request):
 	logout(request)
 	return HttpResponseRedirect(reverse('index'))
