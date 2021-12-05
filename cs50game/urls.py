957443b5e390aa9061bd73a8f7bfbf40235faa10
nathanieladiah <nathanieladiah@gmail.com>
2021-12-04 23:27:48 -0400

    Add cs50game to installed apps and urls

---
+++ cs50game/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path("", views.index, name="cs50game-index"),
+	# path("post/<int:post_id>", views.post, name="cs50game-post"),
+	# path("random", views.random, name="cs50game-random"),
+	# path("new", views.new_post, name="cs50game-new_post"),
+]
--- myblog/settings.py
+++ myblog/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'myblog',
     'book_blog',
     'authapp',
+    'cs50game',
 ]
 
 MIDDLEWARE = [
