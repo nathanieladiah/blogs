3564984ca10b1f93ff29ab65c7f3c795d01099a8
nathanieladiah <nathanieladiah@gmail.com>
2021-12-06 17:55:20 -0400

    Create layout for a blog post
    Created a dummy layout for a blog post including comments.

--- cs50game/views.py
+++ cs50game/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request, 'cs50game/index.html')
+
+def post(request):
+	return render(request, 'cs50game/post.html')
