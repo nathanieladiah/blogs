f432c1653be50b2d7179a5b69d711fe2d3de60e8
nathanieladiah <nathanieladiah@gmail.com>
2021-12-06 20:09:38 -0400

    Populate index page with posts from database
    Add a featured post as well as regular posts to the index page.

--- cs50game/views.py
+++ cs50game/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Cs50gPost
 
 # Create your views here.
 def index(request):
-	return render(request, 'cs50game/index.html')
+	featured_post = Cs50gPost.objects.filter(featured=True).order_by('-featured').first()
+	posts = Cs50gPost.objects.filter(featured=False).all()
+	return render(request, 'cs50game/index.html', {
+		'featured_post': featured_post,
+		'posts': posts
+	})
 
 def post(request):
 	return render(request, 'cs50game/post.html')
