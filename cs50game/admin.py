6b63490ded2e87d859d2d64f81f042d0f54f89dd
nathanieladiah <nathanieladiah@gmail.com>
2021-12-07 02:39:34 -0400

    Display comments from database
    Display all the comments for a particular post, so far nesting one level
    deep works.

--- cs50game/admin.py
+++ cs50game/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Cs50gPost
+from .models import Cs50gPost, Comment
 
 # Register your models here.
 
 admin.site.register(Cs50gPost)
+admin.site.register(Comment)
