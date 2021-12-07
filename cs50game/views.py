1c410e30e0a305d59b05d23a8a8f0e63ec74ec9b
nathanieladiah <nathanieladiah@gmail.com>
2021-12-06 20:30:21 -0400

    Add pagination to index page

--- cs50game/views.py
+++ cs50game/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
 from .models import Cs50gPost
 
 # Create your views here.
 def index(request):
 	featured_post = Cs50gPost.objects.filter(featured=True).order_by('-featured').first()
 	posts = Cs50gPost.objects.filter(featured=False).all()
+
+	paginator = Paginator(posts, 4)
+
+	page_number = request.GET.get('page')
+	page_obj = paginator.get_page(page_number)
+
 	return render(request, 'cs50game/index.html', {
 		'featured_post': featured_post,
-		'posts': posts
+		'page_obj': page_obj
 	})
 
 def post(request):
