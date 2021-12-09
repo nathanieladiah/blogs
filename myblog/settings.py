2ee45923484dbaa312aec6559bb6a85ce6fa66fc
nathanieladiah <nathanieladiah@gmail.com>
2021-12-08 21:24:06 -0400

    Change to default debug and secret_key

--- myblog/settings.py
+++ myblog/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-@w3o0wz^v-6(2#z8!*z*q#141yf7x$goptpzd22nov3_x0t1)u'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-@w3o0wz^v-6(2#z8!*z*q#141yf7x$goptpzd22nov3_x0t1)u'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'TRUE')
+DEBUG = True
+# DEBUG = (os.environ.get('DEBUG_VALUE') == 'TRUE')
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
