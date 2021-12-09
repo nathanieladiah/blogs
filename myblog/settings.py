ed558f7b984855d741498e05c9e70928e993f986
nathanieladiah <nathanieladiah@gmail.com>
2021-12-08 21:36:57 -0400

    Change from default debug = True

--- myblog/settings.py
+++ myblog/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = (os.environ.get('DEBUG_VALUE') == 'TRUE')
+# DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'TRUE')
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['nathanielsblogs.herokuapp.com']
 
 
 # Application definition
