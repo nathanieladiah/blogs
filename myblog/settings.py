ef81e2140cffa43cdadf3607fefe3cac9fa3f342
nathanieladiah <nathanieladiah@gmail.com>
2021-12-20 14:04:30 -0400

    Revert back to tracking debug from environ

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = (os.environ.get('DEBUG_VALUE') == 'TRUE')
+# DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'TRUE')
 
 ALLOWED_HOSTS = ['nathanielsblogs.herokuapp.com', '0.0.0.0', 'localhost']
 
